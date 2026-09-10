f12f742a6789afc659e8446867c10c500a544334
Kishore Gelam <kishore.g@godavarikrishna.com>
2026-09-10 11:43:25 +0530

    fix(cors): update allowed origins for CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -12,13 +12,12 @@ app = FastAPI(title="Complaint Box API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
+        allow_origins=[
         "http://localhost:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
-        "https://yourdomain.com",       # production frontend domain add చేయండి
-        "https://www.yourdomain.com",   # www వెర్షన్ కూడా వాడితే
+        "https://complaint-ebon.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
